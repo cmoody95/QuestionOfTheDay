9a5fb363c6fec0de0db01e65f3a0f651add34a97
Christopher Moody <chris.lee.moody@gmail.com>
2017-11-05 12:07:47 -0600

    Added the two keys from Backendless into the database manager file (viewController.swift).

--- QuestionOfTheDay/ViewController.swift
+++ QuestionOfTheDay/ViewController.swift
@@ -2,8 +2,8 @@
 
 class ViewController: UIViewController {
     
-    let APPLICATION_ID = ""
-    let API_KEY = ""
+    let APPLICATION_ID = "EDC84C5E-00DC-2D93-FFC6-0708BF6D7C00"
+    let API_KEY = "22D5AC3C-6EB8-C790-FFB1-69AEB3B6D600"
     let SERVER_URL = "https://api.backendless.com"
     let backendless = Backendless.sharedInstance()!
     
